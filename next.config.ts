6b71be87e86dcd36e8e3acf47501f2c0f60b5356
LeeGukgeon <lkukun0223@gmail.com>
2025-08-20 11:26:26 +0900

    init

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const prefix =
+  process.env.NODE_ENV === 'production'
+    ? 'https://leegukgeon.github.io'
+    : '';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  assetPrefix: prefix,
+};
+
+export default nextConfig;
